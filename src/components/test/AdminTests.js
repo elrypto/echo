cdfecd7c1cbbe15064aa920e84c5ba3096f3b4cb
lrypto <lloydrebello@gmail.com>
2019-02-14 10:20:33 -0500

    save state on reorder of cards

--- src/components/test/AdminTests.js
+++ src/components/test/AdminTests.js
@@ -11,13 +11,15 @@ import Column from './Column';
 const initialData = {
     tasks : {
       'task-1': {id: 'task-1', content: 'take out garbage'},
-      'task-2': {id: 'task-2', content: 'cook dinner'}
+      'task-2': {id: 'task-2', content: 'cook dinner'},
+      'task-3': {id: 'task-3', content: 'buy groceries'},
+      'task-4': {id: 'task-4', content: 'mow the lawn'}
     },
     columns: {
       'column-1': {
         id: 'column-1',
         title: 'To do',
-        taskIds: ['task-1', 'task-2']
+        taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
       }
     },
     columnOrder: ['column-1']
@@ -39,7 +41,36 @@ class AdminTests extends Component {
   }
 
   onDragEnd = result => {
-    //TODO: reorder codlumn
+    const {destination, source, draggableId} = result;
+
+    if (!destination){
+      return;
+    }
+
+    if (destination.droppableId === source.droppableId &&
+          destination.index === source.index){
+      return;        
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+
+    this.setState(newState);
   }
 
   /*<div>
