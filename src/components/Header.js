41e2e92d9e0c3071723c7ec454117fa43b6f703b
lrypto <lloydrebello@gmail.com>
2019-02-26 15:54:34 -0500

    added loom-js

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,6 @@ class Header extends Component {
                 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
                     <h5 class="my-0 mr-md-auto font-weight-normal"><a class="p-2 text-dark" href="/">echo</a></h5>
                     <nav class="my-2 my-md-0 mr-md-3">
-                        <a class="p-2 text-dark" href="https://github.com/lrypto/0x-hack">Demo Instructions</a>
                         <a class="p-2 text-dark" href="#/leaderboard">Leaderboard</a>
                         <a class="p-2 text-dark" href="#/my_portfolio">My Portfolios</a>
                     </nav>
--- src/views/Leaderboard.js
+++ src/views/Leaderboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
-import LeaderBoardImg from './leaderboard.png';
+
 
 
 class Leaderboard extends Component {
@@ -13,7 +13,7 @@ class Leaderboard extends Component {
         
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h4 class="display-5">leaderboard</h4>
-          <img src={LeaderBoardImg}/>
+         
         </div>
 
         <Footer />
