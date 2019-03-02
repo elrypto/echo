6f9d3102190923bdd3c06bdf06c267b78d8b068c
lrypto <lloydrebello@gmail.com>
2019-03-02 18:38:47 -0500

    fixed calls to write to loom (and read)

--- src/loom-web3-test.js
+++ src/loom-web3-test.js
@@ -3,36 +3,58 @@ const { readFileSync } = require('fs');
 const LoomTruffleProvider = require('loom-truffle-provider');
 const {CryptoUtils, Client, LoomProvider, LocalAddress} = require('loom-js');
 
-const privateKey = CryptoUtils.generatePrivateKey();
-const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey);
 
-// Create the client
-const client = new Client(
-  'default',
-  'ws://127.0.0.1:46658/websocket',
-  'ws://127.0.0.1:46658/queryws',
-);
+// This function will initialize and return the client
+function getClient(privateKey, publicKey) {
+  const client = new Client(
+    'default',
+    'ws://127.0.0.1:46658/websocket',
+    'ws://127.0.0.1:46658/queryws',
+  )
 
-// The address for the caller of the function
-const from = LocalAddress.fromPublicKey(publicKey).toString();
+  return client
+}
 
-// Instantiate web3 client using LoomProvider
-const web3 = new Web3(new LoomProvider(client, privateKey));
+// Setting up keys
+const privateKey = CryptoUtils.generatePrivateKey()
+const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey)
 
-const ABI = [{"anonymous":false,"inputs":[{"indexed":false,"name":"_value","type":"uint256"}],"name":"NewValueSet","type":"event"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
+// Client ready
+const client = getClient(privateKey, publicKey)
 
-const contractAddress = '0x7c40eFe4E2E980C0236adBa8032430901680339E';
+// Setting the web3
+const web3 = new Web3(new LoomProvider(client, privateKey))
 
-// Instantiate the contract and let it ready to be used
-const contract = new web3.eth.Contract(ABI, contractAddress, {from});
+;(async () => {
+  // Set the contract ABI
+  const ABI = [{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]
 
+  // Getting our address based on public key
+  const fromAddress = LocalAddress.fromPublicKey(publicKey).toString()
 
-(async function () {
+  // Get the contract address (we don't need to know the address just the name specified in genesis.json
+  const loomContractAddress = await client.getContractAddressAsync('SimpleStore')
+
+  // Translate loom address to hexa to be compatible with Web3
+  const contractAddress = CryptoUtils.bytesToHexAddr(loomContractAddress.local.bytes)
+
+  // Instantiate the contract
+  const contract = new web3.eth.Contract(ABI, contractAddress, {from: fromAddress})
+
+  // Listen for new value set
+  contract.events.NewValueSet({}, (err, newValueSet) {
+    if (err) {
+      console.error('error', err)
+      return
+    }
+
+    console.log('New value set', newValueSet.returnValues)
+  })
+
   // Set value of 47
-  await contract.methods.set(17).send()
+  await contract.methods.set(47).send()
 
   // Get the value
   const result = await contract.methods.get().call()
-  console.log("result from loom call:" + result);
   // result should be 47
 })()
