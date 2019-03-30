60d8908cdfa826a525f453f9e2aa9fc3908a4a08
lrypto <lloydrebello@gmail.com>
2019-03-30 15:49:39 -0400

    added solidity contract to register, and tests

--- contracts/EchoRegister.sol
+++ contracts/EchoRegister.sol
@@ -2,28 +2,40 @@ pragma solidity ^0.5.0;
 
 
 contract EchoRegister {
-  mapping (address => string) ownerToIndexName;
-  mapping (uint => address) registered;
+  mapping (address => string) registeredToIndexName;
+  mapping (uint => address) registeredToAddress;
   uint registeredCount;
 
- 
   event indexRegistered(string _name, address _address);
   
   modifier indexMustExist(){
-    bytes memory emptyStringTest = bytes(ownerToIndexName[msg.sender]);
+    bytes memory emptyStringTest = bytes( registeredToIndexName[msg.sender]);
     require(emptyStringTest.length != 0, " Index must exist, call createIndex() and initialize first.");
     _;
   }
 
-  
+
+  function getAllAddresses() view public returns (address[] memory) {
+    address[] memory result = new address[](registeredCount);
+    for (uint i = 0; i < registeredCount; i++){
+        result[i] = registeredToAddress[i+1];
+    }
+    return result;
+  }
 
 
   function createIndex(string memory _name) public {
-    ownerToIndexName[msg.sender] = _name;
+    registeredToIndexName[msg.sender] = _name;
+    registeredCount++;
+    registeredToAddress[registeredCount] = msg.sender;
     emit indexRegistered(_name, msg.sender);
   }
 
   function getIndexName() public view returns (string memory) {
-    return ownerToIndexName[msg.sender];
+    return registeredToIndexName[msg.sender];
+  }
+
+  function getAddressForId(uint _id) public view returns (address){
+    return registeredToAddress[_id];
   }
 }
--- test/echoregister_test..js
+++ test/echoregister_test..js
@@ -13,71 +13,26 @@ contract("EchoRegister", accounts => {
 
 
 contract("EchoRegister", accounts => {
-  it("...should store the index name:" + testval, async () => {
+  it("...expect that will return accounts[0] for this:" + testval, async () => {
     const echoInstance = await Echo.deployed();
     await echoInstance.createIndex(testval, { from: accounts[0] });
-    const storedData = await echoInstance.getIndexName.call();
-    assert.equal(storedData, testval, "The value:" + testval + " was not stored.");
+    const address = await echoInstance.getAddressForId(1);
+    //console.log("address=" + address);
+    assert.equal(accounts[0], address, "The value:" + accounts[0] + " was not returned.");
   });
 });
 
 
-/*
-contract("Echo", accounts => {
-  it("...should store the index name:" + testval, async () => {
+contract("EchoRegister", accounts => {
+  it("...3 accounts returned:" + testval, async () => {
     const echoInstance = await Echo.deployed();
-    await echoInstance.createIndex(testval, { from: accounts[0] });
-    const storedData = await echoInstance.getIndexName.call();
-    assert.equal(storedData, testval, "The value:" + testval + " was not stored.");
-  });
-
-  it("...should addToken ZRX", async () => {
-    const echoInstance = await Echo.deployed();
-    await echoInstance.createIndex(testval, { from: accounts[0] });
-    await echoInstance.addToken(zrx, 10, { from: accounts[0] });
-    let token  = await echoInstance.getToken(0);
-    assert.equal(token[0], zrx, "The token " + zrx + " was not stored.");
-  });
-
-});
-
-contract("Echo", accounts => {
-  it("...should have a token count of 1", async () => {
-    const echoInstance = await Echo.deployed();
-    await echoInstance.createIndex(testval, { from: accounts[0] });
-    await echoInstance.addToken(zrx, 10, { from: accounts[0] });
-    let count = await echoInstance.tokenCount({ from: accounts[0] });
-    assert.equal(1, count, "The token could should have been 1.");
-  });
-
-    it("...should check that account was created", async () => {
-      //use modifier?
-      const echoInstance = await Echo.deployed();
-     
-    });
-});
-
-
-contract("Echo", accounts => {
-  it("...should get 3 tokens that were just added", async () => {
-    const echoInstance = await Echo.deployed();
-    await echoInstance.createIndex(testval, { from: accounts[0] });
-    await echoInstance.addToken(zrx, 1, { from: accounts[0] });
-    await echoInstance.addToken(mkr, 1, { from: accounts[0] });
-    await echoInstance.addToken(weth, 1, { from: accounts[0] });
-    let count = await echoInstance.tokenCount({ from: accounts[0] });
-    assert.equal(3, count, "The token could should have been 3.");
-
-    let result = await echoInstance.tokensForOwner(accounts[0]);
-    let res1 = await echoInstance.getToken(result[0]);
-    let res2 = await echoInstance.getToken(result[1]);
-    let res3 = await echoInstance.getToken(result[2]);
-    
-    assert.equal(zrx, res1[0], "expected zrx in first slot");
-    assert.equal(mkr, res2[0], "expected mkr in first slot");
-    assert.equal(weth, res3[0], "expected weth in first slot");
+    await echoInstance.createIndex("first index", { from: accounts[0] });
+    await echoInstance.createIndex("second index", { from: accounts[1] });
+    await echoInstance.createIndex("third index", { from: accounts[2] });
+    const all = await echoInstance.getAllAddresses();
+    assert.equal(accounts[0], all[0], "The value:" + accounts[0] + " was not returned.");
+    assert.equal(accounts[1], all[1], "The value:" + accounts[1] + " was not returned.");
+    assert.equal(accounts[2], all[2], "The value:" + accounts[2] + " was not returned.");
   });
 });
 
-
-*/
