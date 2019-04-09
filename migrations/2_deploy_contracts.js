cb3db2b2eb11c0e7a60f513bd2f37d531ac1eaa5
lrypto <lloydrebello@gmail.com>
2019-04-09 13:16:00 -0400

    cleaned up contracts and tests migration to echo-app-contract repo

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var Echo = artifacts.require("./Echo.sol");
 var EchoRegister = artifacts.require("./EchoRegister.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Echo);
   deployer.deploy(EchoRegister);
 };
